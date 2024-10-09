fdbc4c890db0bdc76694e4dfd7bd9dcc19f67e65
Mario Micklisch <mario@favo.org>
2024-10-09 11:44:16 +0200

    Update vechain sdk for cloudflare worker support

--- apps/website/src/hooks/useBeats.ts
+++ apps/website/src/hooks/useBeats.ts
@@ -1,6 +1,6 @@
 import React from 'react';
 import { subscriptions } from '@vechain/sdk-network';
-import { bloomUtils, addressUtils } from '@vechain/sdk-core';
+// import { bloomUtils, addressUtils } from '@vechain/sdk-core';
 import useWebSocket from 'react-use-websocket';
 import { NODE_URL } from '~/config';
 
@@ -19,6 +19,10 @@ type Beat = {
 const DELAY = 100;
 
 /**
+ * @TODO: hook is currently disabled, until https://github.com/vechain/vechain-sdk-js/issues/1359 is updated
+ */
+
+/**
  * Subscribe to blockchain updates and filter them based on provided addresses, transactions or data.
  *
  * @param {string[]} addressesOrData - An array of addresses, transaction ids or data to filter the incoming beats.
@@ -26,32 +30,32 @@ const DELAY = 100;
  */
 const useBeats = (addressesOrData: (string | `0x${string}` | null | undefined)[]) => {
   const [block, setBlock] = React.useState<Beat | null>(null);
-  const { lastJsonMessage } = useWebSocket(
-    subscriptions.getBeatSubscriptionUrl(NODE_URL),
-    {
-      share: true,
-      shouldReconnect: () => true,
-    }
-  );
+  // const { lastJsonMessage } = useWebSocket(
+  //   subscriptions.getBeatSubscriptionUrl(NODE_URL),
+  //   {
+  //     share: true,
+  //     shouldReconnect: () => true,
+  //   }
+  // );
 
-  React.useEffect(() => {
-    try {
-      const block = lastJsonMessage as Beat;
-      if (
-        (addressesOrData
-          .filter(value => Boolean(value)) as string[])
-          .some((addressOrData: string) =>
-            addressUtils.isAddress(addressOrData)
-              ? bloomUtils.isAddressInBloom(block.bloom, block.k, addressOrData)
-              : bloomUtils.isInBloom(block.bloom, block.k, addressOrData)
-          )
-      ) {
-        setTimeout(() => setBlock(block), DELAY);
-      }
-    } catch {
-      /* ignore */
-    }
-  }, [lastJsonMessage]);
+  // React.useEffect(() => {
+  //   try {
+  //     const block = lastJsonMessage as Beat;
+  //     if (
+  //       (addressesOrData
+  //         .filter(value => Boolean(value)) as string[])
+  //         .some((addressOrData: string) =>
+  //           addressUtils.isAddress(addressOrData)
+  //             ? bloomUtils.isAddressInBloom(block.bloom, block.k, addressOrData)
+  //             : bloomUtils.isInBloom(block.bloom, block.k, addressOrData)
+  //         )
+  //     ) {
+  //       setTimeout(() => setBlock(block), DELAY);
+  //     }
+  //   } catch {
+  //     /* ignore */
+  //   }
+  // }, [lastJsonMessage]);
 
   return block;
 };
